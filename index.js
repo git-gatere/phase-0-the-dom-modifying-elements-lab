76d7f74a3acc1a15e71965c881a3fdffa4d613bd
gatere <mercynjerigatere@gmail.com>
2024-04-17 02:00:06 -0400

    Completes DOM lab

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ document.querySelector("main#main").remove();
 
 
 const newHeader = document.createElement("h1");
-newHeader.id = "randomer"
-newHeader.innerHTML = "random";
-newHeader.className = "randomer";
+newHeader.id = "victory"
+newHeader.innerHTML = "is the champion";
+newHeader.className = "victory";
