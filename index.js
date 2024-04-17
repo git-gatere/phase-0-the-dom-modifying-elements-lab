468a9952ab640081c53e49f86dc5c281a7671ce0
gatere <mercynjerigatere@gmail.com>
2024-04-17 01:57:48 -0400

    Adds DOM Lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+const testVar = "yo"
+
+
+document.querySelector("main#main").remove();
+
+
+const newHeader = document.createElement("h1");
+newHeader.id = "randomer"
+newHeader.innerHTML = "random";
+newHeader.className = "randomer";
